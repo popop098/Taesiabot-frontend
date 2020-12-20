6ba0519247f2413c53569b798d0e35a26a4beee5
TFAM_IS_LUV <53284350+popop098@users.noreply.github.com>
2020-12-20 19:36:33 +0900

    Update Team.js

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -19,7 +19,7 @@ const Members = [
 function Team() {
     return (
         <div className='Team'>
-            <h1 className='Developer'>태시아봇 개발자</h1>
+            <h1 className='Developer'>태시아봇 개발자 </h1>
             <Card.Group itemsPerRow={1} stackable>
                 {Members.map(({ name, avatar, id }) => (
                     <Card href={`https://discord.com/users/${id}`} target='_blank' key={id}>
