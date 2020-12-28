5edd7d5069e49506f3f962fd213379c0144a50b0
TFAM_IS_LUV <53284350+popop098@users.noreply.github.com>
2020-12-28 19:03:45 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,49 +5,51 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <body>
-      <script>
-        (function() {
-          var w = window;
-          if (w.ChannelIO) {
-            return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
-          }
-          var ch = function() {
-            ch.c(arguments);
-          };
-          ch.q = [];
-          ch.c = function(args) {
-            ch.q.push(args);
-          };
-          w.ChannelIO = ch;
-          function l() {
-            if (w.ChannelIOInitialized) {
-              return;
-            }
-            w.ChannelIOInitialized = true;
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
-            s.charset = 'UTF-8';
-            var x = document.getElementsByTagName('script')[0];
-            x.parentNode.insertBefore(s, x);
-          }
-          if (document.readyState === 'complete') {
-            l();
-          } else if (window.attachEvent) {
-            window.attachEvent('onload', l);
-          } else {
-            window.addEventListener('DOMContentLoaded', l, false);
-            window.addEventListener('load', l, false);
-          }
-        })();
-        ChannelIO('boot', {
-          "pluginKey": "dddccf59-a91b-493a-9e29-0aa16e96e800"
-        });
-      </script>
-    </body>
     <div className='home' onDragStart={event => event.preventDefault()}>
+    <body>
+    <!-- Channel Plugin Scripts -->
+    <script>
+      (function() {
+        var w = window;
+        if (w.ChannelIO) {
+          return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+        }
+        var ch = function() {
+          ch.c(arguments);
+        };
+        ch.q = [];
+        ch.c = function(args) {
+          ch.q.push(args);
+        };
+        w.ChannelIO = ch;
+        function l() {
+          if (w.ChannelIOInitialized) {
+            return;
+          }
+          w.ChannelIOInitialized = true;
+          var s = document.createElement('script');
+          s.type = 'text/javascript';
+          s.async = true;
+          s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+          s.charset = 'UTF-8';
+          var x = document.getElementsByTagName('script')[0];
+          x.parentNode.insertBefore(s, x);
+        }
+        if (document.readyState === 'complete') {
+          l();
+        } else if (window.attachEvent) {
+          window.attachEvent('onload', l);
+        } else {
+          window.addEventListener('DOMContentLoaded', l, false);
+          window.addEventListener('load', l, false);
+        }
+      })();
+      ChannelIO('boot', {
+        "pluginKey": "dddccf59-a91b-493a-9e29-0aa16e96e800"
+      });
+    </script>
+    <!-- End Channel Plugin -->
+    </body>
       <img src={Teasia} className='Img' alt='kaibot'></img>
       <h4 className='Desc'>태시아봇은 서버관리와 뮤직, 웹검색,<br /> 자동자가진단등 유용한기능들을<br /> 가지고있는 다기능 봇입니다.</h4>
       <div className='btn-container'>
